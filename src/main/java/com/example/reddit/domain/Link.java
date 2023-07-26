0e80fa274275e04c1d061f0a3ba2fd63771f9222
Malak <malaklabibamer@gmail.com>
2023-07-26 12:29:06 +0300

    added mappings to link and comment

--- src/main/java/com/example/reddit/domain/Link.java
+++ src/main/java/com/example/reddit/domain/Link.java
@@ -2,11 +2,13 @@ package com.example.reddit.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -19,4 +21,6 @@ public class Link {
     private String title;
     private String url;
 
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
